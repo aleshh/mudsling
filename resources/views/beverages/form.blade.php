9d184147f4f765052a24d85e9e6725908a1a0b69
Alesh Houdek <mail@alesh.com>
2018-03-04 12:58:42 -0700

    form remembers category, selects it from dropdown

--- resources/views/beverages/form.blade.php
+++ resources/views/beverages/form.blade.php
@@ -8,18 +8,21 @@
 <form method="POST" class="input-form" action="/beverages" >
 
   @if($beverage->id)
-    <p>patch!</p>
     @method('PATCH')
     <input type="hidden" value="{{ $beverage->id }}" name="id" id="id">
   @endif
 
   @csrf
 
-  <select  value="{{ $beverage->category }}" name="category" id="category">
-    <option value="Beer">Beer</option>
-    <option value="Wine">Wine</option>
-    <option value="Liquor">Liquor</option>
-    <option value="Cocktail">Cocktail</option>
+  <select name="category" id="category">
+
+    @foreach(['Beer', 'Wine', 'Liquor', 'Cocktail'] as $c)
+      <option value="{{ $c }}"
+        @if(old('category', $beverage->category) == $c)
+          selected="selected"
+        @endif
+      >{{ $c }}</option>
+    @endforeach
   </select>
 
   <div>
@@ -29,17 +32,17 @@
       name="name"
       id="name"
       placeholder="Name of Drink"
-      value="{{ $beverage->name }}">
+      value="{{ old('name', $beverage->name) }}">
   </div>
 
   <div>
     <label for="size">Size</label>
-    <input value="{{ $beverage->size }}" type="text" name="size" id="size" placeholder="Ounces">
+    <input value="{{ old('size', $beverage->size) }}" type="text" name="size" id="size" placeholder="Ounces">
   </div>
 
   <div>
     <label for="strength">Strength</label>
-    <input  value="{{ $beverage->strength }}" type="text" name="strength" id="strength" placeholder="% alcohol">
+    <input  value="{{ old('strength', $beverage->strength) }}" type="text" name="strength" id="strength" placeholder="% alcohol">
   </div>
 
   <button type="submit" class="submit-button">Submit</button>
