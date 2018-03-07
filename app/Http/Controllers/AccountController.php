3560f78f1a999f0bad3cf8484d637953eb6a2525
Alesh Houdek <mail@alesh.com>
2018-03-07 16:30:28 -0700

    user account screen hour and minute adjustment communicates back to controller

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -3,16 +3,39 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Carbon\Carbon;
 
 class AccountController extends Controller
 {
     public function index() {
-        return view('account');
+
+        $hour = now()->hour;
+        if ($hour > 12) {
+          $hour -= 12;
+          $amPm = "pm";
+        } else {
+          $amPm = "am";
+        }
+
+        $minute = now()->minute;
+        if ($minute < 10) {
+          $minute = "0".$minute;
+        }
+
+        return view('account', compact('hour', 'minute', 'amPm'));
     }
 
     public function update(Request $request) {
         $user = \Auth::user();
         $user->maximumConsumption = $request['maximumConsumption'];
+
+        $hourDiff   = $request['userHour'] - $request['originalHour'];
+        $minuteDiff = $request['userMinute'] - $request['originalMinute'];
+
+        dd($hourDiff.":".$minuteDiff);
+        $userTime = new Carbon($request['userTime']);
+
+        dd("change!");
         $user->save();
 
         return redirect('/');
--- resources/views/account.blade.php
+++ resources/views/account.blade.php
@@ -3,20 +3,39 @@
 
 @section('content')
 
-<h2>{{ \Auth::user()->name }}</h2>
+  <h2>{{ \Auth::user()->name }}</h2>
 
   <form method="POST" class="input-form border-bottom" action="/account" >
 
     @method('PATCH')
     @csrf
 
-    <label for="maximumConsumption">Daily Maximum Alcohol Target</label>
-    <input value="{{ \Auth::user()->maximumConsumption }}" type="text" name="maximumConsumption" id="maximumConsumption" placeholder="in Oz">
+    <label for="maximumConsumption">Daily Maximum Alcohol Target (oz.)</label>
+    <input
+      value="{{ \Auth::user()->maximumConsumption }}"
+      type="text"
+      name="maximumConsumption"
+      id="maximumConsumption"
+      placeholder="in Oz">
 
+    <p style="margin-bottom: 25px">
+      For example, three 12oz. beers with 5% alcohol:<br>
+      3 × 12 × .05 = 1.8
+    </p>
+
+    <label >Local Time</label>
+    <input value="{{ $hour }}"   type="text" name="userHour"   style="text-align:center;display:inline-block;width: 50px;"> :
+    <input value="{{ $minute }}" type="text" name="userMinute" style="text-align:center;display:inline-block;width: 50px;">
+    {{ $amPm }}
+
+    <input value="{{ $hour }}"   type="hidden" name="originalHour"   >
+    <input value="{{ $minute }}" type="hidden" name="originalMinute" >
+    <input value="{{ $amPm }}" type="hidden" name="originalAmPm" >
+    <br>
     <button type="submit" class="submit-button">Submit</button>
   </form>
 
-  <h3>Logout</h3>
+  <h2>Logged in</h2>
   <p>
     <a class="submit-button" href="/logout">Logout</a>
   </p>
